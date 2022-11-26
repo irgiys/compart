5e0622f0e7f59d99858b14a23a3037b4d78ddf11
iynsys <irgiyansy@gmail.com>
2022-11-27 03:56:23 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <link rel="stylesheet" href="css/custom.min.css" />
+    </head>
+    <body>
+        <div class="overflow-hidden container-fluid">
+            <div class="row min-vw-100 min-vh-100">
+                <div class="col-5 bg-primary">
+                    Discover the world's top Hardware
+                </div>
+                <div class="col-7">
+                    <form action="" method="post">
+                        <div class="d-flex flex-column justify-content-center vh-100">
+                            <h1>Sign In</h1>
+                            <div>
+                                <label for="username">
+                                    Username 
+                                </label>
+                                <input class="form-control" type="text" id="username" name="username">
+                            </div>
+                            <div>
+                                <label for="password">
+                                    Password 
+                                </label>
+                                <input class="form-control" type="password" id="password" name="password">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body>
+</html>
